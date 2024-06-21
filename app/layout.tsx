32413fabc8f27832b537d73864d87315d6426043
John Doe <john@sample.com>
2024-06-22 01:31:57 +0530

    clerk authentication done

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,38 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
-
-const inter = Inter({ subsets: ["latin"] });
-
-export const metadata: Metadata = {
-  title: "Admin Dashboard",
-  description: "Admin Dashboard",
-};
+import { ClerkProvider, SignInButton, SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
+import './globals.css';
 
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+}: {
+  children: React.ReactNode
+}) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  );
+    <ClerkProvider>
+      <html lang="en">
+        <body>
+          <header>
+            <UserButton showName />
+          </header>
+          <main>
+            <SignedOut>
+              <SignInButton />
+            </SignedOut>
+            <SignedIn>
+              {children}
+            </SignedIn>
+          </main>
+        </body>
+      </html>
+    </ClerkProvider>
+  )
 }
+
+
+
+import { clerkMiddleware } from "@clerk/nextjs/server";
+
+clerkMiddleware();
+
+export const config = {
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+};
